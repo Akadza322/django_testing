ff22b68e03d275550bec2615ddc79ce101f83a11
Akadza322 <tumanov240360@mail.ru>
2024-11-13 07:46:13 +0300

    minor edits

--- tests/students/test_courses_api.py
+++ tests/students/test_courses_api.py
@@ -4,9 +4,6 @@ from students.models import Course, Student
 from rest_framework.test import APIClient
 import random
 
-def test_example():
-    assert False, "Just test example"
-
 
 @pytest.fixture
 def client():
@@ -47,6 +44,37 @@ def test_curses_list(client, curs_factory):
     for i, c in enumerate(data_list):
         assert c['name'] == curs_list[i].name
 
+
+@pytest.mark.django_db
+def test_filter_id_courses(client, curs_factory):
+    list_curses = (
+        curs_factory(_quantity=10)
+    )
+    r = (
+        random.randint(0,9)
+    )
+    id_curse = list_curses[r].id
+
+    response = client.get(f'/courses/?id={id_curse}')
+    data = response.json()
+
+    assert response.status_code == 200
+    assert data[0]['name'] == list_curses[r].name
+
+
+@pytest.mark.django_db
+def test_filter_name_courses(client, curs_factory):
+    list_curses = curs_factory(_quantity=10)
+    r = random.randint(0,9)
+    name_curse = list_curses[r].name
+
+    response = client.get(f'/courses/?name={name_curse}')
+    data = response.json()
+
+    assert response.status_code == 200
+    assert data[0]['name'] == list_curses[r].name
+
+
 @pytest.mark.django_db
 def test_create_course(client):
     count_before = Course.objects.count()
@@ -61,10 +89,10 @@ def test_create_course(client):
 
 
 @pytest.mark.django_db
-def test_patch_course(client, courses_factory):
-    list_courses = courses_factory(_quantity=10)
+def test_patch_course(client, curs_factory):
+    list_curses = curs_factory(_quantity=10)
     r = random.randint(0,9)
-    id_course = list_courses[r].id
+    id_course = list_curses[r].id
     data = {
         'name': 'python',
         'students': []
@@ -78,8 +106,8 @@ def test_patch_course(client, courses_factory):
 
 
 @pytest.mark.django_db
-def test_delete_course(client, courses_factory):
-    list_courses = courses_factory(_quantity=10)
+def test_delete_course(client, curs_factory):
+    list_courses = curs_factory(_quantity=10)
     r = random.randint(0, 9)
     id_course = list_courses[r].id
     count_before = Course.objects.count()
